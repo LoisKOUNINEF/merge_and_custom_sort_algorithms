a03e5127fed67af37db81dd7aa9594a8a8bfaf60
LoisKOUNINEF <lkouninef@gmail.com>
2022-04-20 09:32:22 +0200

    merge sort OK

--- merge.js
+++ merge.js
@@ -14,3 +14,40 @@ catch (error) {
 }
 
 console.log(`Initial array : ${dataArray}`)
+
+class Result {
+
+  constructor(arr) {
+    this.counter = 0;
+    this.sortedArray = this.mergeSort(arr);
+  }
+
+  mergeSort(arr) {
+    const halfArray = arr.length / 2;
+    if (arr.length <= 1) {
+      return arr;
+    }
+    const firstHalf = arr.splice(0, halfArray);
+    const secondHalf = arr;
+    return this.merge(this.mergeSort(firstHalf), this.mergeSort(secondHalf))
+  }
+
+  merge(firstHalf, secondHalf) {
+    let sortedArray = [];
+    while (firstHalf.length && secondHalf.length) {
+      this.counter++;
+      if (firstHalf[0] < secondHalf[0]) {
+        sortedArray.push(firstHalf.shift());
+      }
+      else {
+        sortedArray.push(secondHalf.shift());
+      }
+    }
+    return [...sortedArray, ...firstHalf, ...secondHalf];
+  }
+}
+
+let mergeResult = new Result(dataArray);
+
+console.log(`Merge sort algorythm : ${mergeResult.counter} comparisons.`)
+console.log(mergeResult.sortedArray)
